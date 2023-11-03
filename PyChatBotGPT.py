909b52386224ef1f02a4f67c2a5d4d9db4adb496
Dynamic Mushroom <78193555+DynamicMushroom@users.noreply.github.com>
2023-11-02 20:36:53 -0500

    Update PyChatBotGPT.py
    Fixes

--- PyChatBotGPT.py
+++ PyChatBotGPT.py
@@ -6,30 +6,23 @@ import logging
 import boto3
 from dotenv import load_dotenv
 from flask_cors import CORS
+import sys
 
 # Load environment variables
 load_dotenv()
 
-# Enter your dotenv file name with your API key and AWS credentials
+# Enter your dotenv file name with your API key
 openai.api_key = os.getenv("OPENAI_API_KEY")
-aws_access_key_id = os.getenv("AWS_ACCESS_KEY_ID")
-aws_secret_access_key = os.getenv("AWS_SECRET_ACCESS_KEY")
-aws_region = os.getenv("AWS_REGION")
-s3_bucket_name = os.getenv("S3_BUCKET_NAME")
-s3_key_name = os.getenv("S3_KEY_NAME")
 
-# Setup logging
-logging.basicConfig(filename='chat.log', level=logging.INFO)
+# Setup logging to console instead of a file
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
 # Flask and CORS app setup
 app = Flask(__name__)
 CORS(app, resources={r"/ask": {"origins": "*"}})
 
-# AWS S3 Setup
-s3 = boto3.client('s3',
-                  aws_access_key_id=aws_access_key_id,
-                  aws_secret_access_key=aws_secret_access_key,
-                  region_name=aws_region)
+# AWS S3 Setup (configured for Cyclic's S3 storage)
+s3 = boto3.client('s3')
 
 model_engine = "gpt-4"
 model_prompt = "You are GPT-4, a large language model trained by OpenAI. Help answer questions and engage in conversation."
@@ -40,16 +33,16 @@ max_history_tokens = 2000
 def save_history(chat_history):
     s3.put_object(
         Body=json.dumps(chat_history),
-        Bucket=s3_bucket_name,
-        Key=s3_key_name
+        Bucket="cyclic-eager-battledress-yak-us-east-1",  # Replace with your actual bucket name
+        Key="chat_history/my_chat_history.json"
     )
         
 # Load history function
 def load_history():
     try:
         my_file = s3.get_object(
-            Bucket=s3_bucket_name,
-            Key=s3_key_name
+            Bucket="cyclic-eager-battledress-yak-us-east-1",  # Replace with your actual bucket name
+            Key="chat_history/my_chat_history.json"
         )
         return json.loads(my_file['Body'].read())
     except s3.exceptions.NoSuchKey:
@@ -96,4 +89,4 @@ def ask():
 # Main block to run the Flask app
 if __name__ == "__main__":
     chat_history = load_history()  # Load chat history from S3
-    app.run(port=3000)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 3000)))
