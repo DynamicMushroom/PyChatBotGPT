a8fc1903413054b1df35e7fc3ec70ae12017db8e
Dynamic Mushroom <78193555+DynamicMushroom@users.noreply.github.com>
2023-11-02 20:09:17 -0500

    Update PyChatBotGPT.py
    Before AWS addition

--- PyChatBotGPT.py
+++ PyChatBotGPT.py
@@ -1,61 +1,82 @@
+from flask import Flask, request, jsonify
 import os
 import openai
+import json
+import logging
 from dotenv import load_dotenv
+from flask_cors import CORS
 
+# Load environment variables
 load_dotenv()
 
+#Enter your dotenv file name with your API key
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-# Set up the OpenAI model
-model_engine = "davinci-codex"  # choose the model engine you want to use
-model_prompt = "You are ChatGPT, a large language model trained by OpenAI. Help answer questions and engage in conversation."  # prompt to start the conversation
-chat_history = []  # keep track of the conversation history
-max_history_tokens = 1500  # maximum number of tokens to keep in the chat history
+# Setup logging
+logging.basicConfig(filename='chat.log', level=logging.INFO)
 
+# Flask and CORS app setup
+app = Flask(__name__)
+CORS(app, resources={r"/ask": {"origins": "*"}})
+
+
+model_engine = "gpt-4"
+model_prompt = "You are GPT-4, a large language model trained by OpenAI. Help answer questions and engage in conversation."
+chat_history = []
+max_history_tokens = 2000
+
+#save history function
+def save_history(chat_history, filename='history.json'):
+    with open(filename, 'w') as f:
+        json.dump(chat_history, f)
+        
+ #Load history function
+def load_history(filename='history.json'):
+    with open(filename, 'r') as f:
+        return json.load(f)
+    
+#Response
 def generate_response(prompt, model_engine, chat_history):
-    # Check if the prompt is empty or contains only whitespace
     if not prompt.strip():
         return ""
 
-    # Prepare conversation history for the API
     conversation = "".join([f"{entry}\n" for entry in chat_history])
 
-    # Generate a response using OpenAI's API
-    response = openai.Completion.create(
-        engine=model_engine,
-        prompt=f"{model_prompt}\n{conversation}User: {prompt}\nAssistant:",
-        temperature=0.5,
-        max_tokens=1024,
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0,
-        stop=["\n"]
-    )
-
-    # Decode the response and remove the newline characters
-    text = response.choices[0].text
-    text = text.strip()
-
-    # Add the response to the chat history
-    chat_history.append(f"Assistant: {text}")
-
+    try:
+        response = openai.ChatCompletion.create(
+            model=model_engine,
+            messages=[
+                {"role": "system", "content": model_prompt},
+                {"role": "user", "content": f"{conversation}User: {prompt}"}
+            ],
+            max_tokens=2000,
+            temperature=0.5,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0,
+            stop=["\n"]
+        )
+        text = response['choices'][0]['message']['content']
+        text = text.strip()
+        chat_history.append(f"Assistant: {text}")
+    except openai.error.OpenAIError as e:
+        text = f"Error: {e}"
+        logging.error(text)
+    
     return text
 
-# Start the conversation
-print(model_prompt)
-while True:
-    # Get user input
-    user_input = input("> ")
-
-    # Add user input to the chat history
-    chat_history.append(f"User: {user_input}")
-
-    # Truncate the chat history if necessary
-    if len("".join(chat_history)) > max_history_tokens:
-        chat_history.pop(0)
-
-    # Generate a response
+# Flask route to handle POST requests
+@app.route('/ask', methods=['POST'])
+def ask():
+    user_input = request.form['user_input']
     response = generate_response(user_input, model_engine, chat_history)
+    return jsonify({'response': response})
 
-    # Print the response
-    print(response)
+# Main block to run the Flask app
+if __name__ == "__main__":
+    try:
+        chat_history = load_history()
+    except FileNotFoundError:
+        pass
+    
+    app.run(port=3000)
